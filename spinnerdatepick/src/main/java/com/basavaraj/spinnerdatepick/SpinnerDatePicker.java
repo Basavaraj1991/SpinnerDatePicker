9a2885e36ca7fcc50f26680d4b9fd8e2cf6e868c
basavarajnavi <basavaraj.navi@coveiot.com>
2017-12-27 16:49:35 +0530

    new changes

--- spinnerdatepick/src/main/java/com/basavaraj/spinnerdatepick/SpinnerDatePicker.java
+++ spinnerdatepick/src/main/java/com/basavaraj/spinnerdatepick/SpinnerDatePicker.java
@@ -14,7 +14,6 @@ import java.util.Calendar;
 /**
  * Created by Basavaraj Navi on 27/12/17.
  * Project SpinnerDatePicker
- * Copyright (c) 2017 KaHa Technologies Pvt Ltd. All rights reserved.
  */
 
 public class SpinnerDatePicker implements DatePicker.OnDateChangedListener{
